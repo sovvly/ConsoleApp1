6ee20ea8c5b1dfd1ea27d2c88b9c5d929d76425d
Przemek Grochowski <sovvly@protonmail.com>
2019-08-07 22:16:33 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Foo(int x, int y)
+        {
+            Console.WriteLine("x = "+ x + " y = " + y);
+        }
+    
+        static void Main(string[] args)
+        {
+            var a = 5;
+            var b = 7;
+            Foo(a, y:b);
+            Console.ReadLine();
+        }
+    }
+
+    class Board
+    {
+        private int _length;
+        private int _width;
+
+
+        public int lth
+        {
+            get { return _length; }
+            set { _length = value; }
+        }
+
+
+    }
+}
+
